1725f3d73c09836a2d993adce43440cbbdb8ca99
Dake Sew <dakesew@yahoo.de>
2015-11-04 13:10:11 +0100

    Changed Layout, added better support for autocompletion

--- Inc/main.h
+++ Inc/main.h
@@ -1,10 +1,10 @@
 /**
   ******************************************************************************
-  * @file    GPIO/GPIO_IOToggle/Inc/stm32f0xx_it.h
+  * @file    GPIO/GPIO_IOToggle/Inc/main.h
   * @author  MCD Application Team
   * @version V1.2.0
   * @date    05-Dec-2014
-  * @brief   This file contains the headers of the interrupt handlers.
+  * @brief   Header for main.c module
   ******************************************************************************
   * @attention
   *
@@ -36,29 +36,20 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F0xx_IT_H
-#define __STM32F0xx_IT_H
+#ifndef __MAIN_H
+#define __MAIN_H
 
-#ifdef __cplusplus
-extern "C" {
+#ifndef STM32F030x6
+#define STM32F030x6
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __STM32F0xx_IT_H */
+#endif /* __MAIN_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Inc/stm32f0xx_it.h
+++ Inc/stm32f0xx_it.h
@@ -1,10 +1,11 @@
+
 /**
   ******************************************************************************
-  * @file    GPIO/GPIO_IOToggle/Inc/main.h
+  * @file    GPIO/GPIO_IOToggle/Inc/stm32f0xx_it.h
   * @author  MCD Application Team
   * @version V1.2.0
   * @date    05-Dec-2014
-  * @brief   Header for main.c module
+  * @brief   This file contains the headers of the interrupt handlers.
   ******************************************************************************
   * @attention
   *
@@ -36,17 +37,29 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __STM32F0xx_IT_H
+#define __STM32F0xx_IT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f0xx_hal.h"
-
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
-#endif /* __MAIN_H */
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void SVC_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F0xx_IT_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
